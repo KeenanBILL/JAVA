abf0d6928d2331ed5af3d2c744fcab5945438970
KeenanBILL <t19008@tsiba.ac.za>
2022-11-15 15:42:32 +0200

    DAY 7

---
+++ index.js
@@ -0,0 +1,197 @@
+//console.log('Hello World1');
+
+//let interestRate=0.3;
+
+//let interest=1;
+
+//console.log (interest);
+
+//const interestRat=0.3;
+
+//console.log(interestRate);
+
+//const person={surname:'Jezel',
+//age: 30,
+//shoesize: 6
+ //};
+
+// let{surname,age,shoesize}=person;
+
+ //console.log(surname);
+
+ //console.log(person.surname);
+
+// let a=10,
+
+  //b=15,
+
+  //c=35; 
+
+  //console.log(a,b,c);
+
+  //console.log ('Keenan+Olivier');
+
+ // const me={firstname:'Keenan', secondname:'Adrian', lastname:'Olivier'};
+
+ // console.log (me.firstname, me.lastname);
+
+//  let numberVAR=5;
+
+ // console.log (numberVAR);
+
+ // let nme = "Keenan";
+//
+ // let sme = "Olivier";
+//
+ // let fme = "Keenan" + "Olivier";
+
+ // console.log(fme);
+
+ // let mental = 50;
+ // let crazy = 'Brazy';
+ // let brazy = true;
+
+ // console.log (typeof mental, crazy, brazy
+  //  );
+
+//const cars = [
+//'Toyota',
+//'BMW',
+//'VW'
+//];
+
+//for (const speed of cars) {
+  //console.log(speed);
+//};
+
+//let mens = {
+//  name: 'Kesha',
+ // surname: 'Reubenheimer',
+ // shoesize: 4
+//};
+
+//for (const peeps in mens) {
+  
+ // console.log(peeps);
+  
+//};
+
+//let i = 1; while (i<11) {
+ // console.log (i);
+  //i++; break;
+//}
+
+// x=5;
+// if (x>2){
+
+// console.log(x +" is greater than 2")
+
+// }else
+
+// {
+
+// console.log(x + " is less than 2")
+
+// };
+
+// let mark=5;
+
+// let result=mark>5 ? "weak":"strong";
+// console.log(result)
+
+// //let a=1;
+// //if(a>10)
+// //{  
+// //    document.write(10);  
+// //} 
+// //else
+// //{
+// //    document.write(a);  
+// //}
+
+// let accessAllowed;
+// let age = 12;
+
+// if (age > 18) {
+//   accessAllowed = true;
+// } else {
+//   accessAllowed = false;
+// }
+
+// console.log (age)
+
+// function testNum(a) {
+//   let result;
+//   if (a > 0) {
+//     result = 'positive';
+//   } else {
+//     result = 'NOT positive';
+//   }
+//   return result;
+// }
+
+// console.log(testNum(-5));
+
+
+// var a = 10;
+// if(a == a++)
+// document.write(a);
+
+// let num1=5;
+// ++num1 +2;
+
+// console.log(num1)
+
+// var x = "0";
+// switch (x) {
+//   case 0:
+//     text = "Off";
+//     break;
+//   case 1:
+//     text = "On";
+//     break;
+//   default:
+//     text = "No value found";
+// }
+
+// console.log(x)
+
+// const sports = ['Football', 'Basketball', 'Rugby', 'Tennis', 'Karate'];
+
+// for (activity of sports) {
+//   console.log(activity)
+//   if (activity === 'Basketball'){
+//     console.log('Basketball is great!');
+//   }
+// }
+
+// for(let i=1; i<21; i++) {
+//   if (i%2===0){
+//     document.write(i+"even number"+"<br/>");
+//   } else {
+//     document.write (i+"odd number"+"<br/>")
+//   }
+// }
+
+// let loading = 0;
+
+// while(loading < 100){
+//   console.log ('Website is still loading');
+
+//   loading++;
+//}
+
+const cars = [
+  'Toyota',
+  'Opel',
+  'Nissan',
+  'Subaru',
+  'Fiat'
+];
+
+let x=0;
+
+while(x < cars.length) {
+  console.log('This ' + (cars[x]) + ' has an index ' + x);
+  x++;
+} 
